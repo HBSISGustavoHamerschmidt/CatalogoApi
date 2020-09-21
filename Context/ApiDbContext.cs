be2f02c5f93093b8cf086fd9ed7952fdc3796d33
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-21 09:31:50 -0300

    Add project files.

---
+++ Context/ApiDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Api.Hbsis.Models.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> optionsDb) : base(optionsDb)
+        {
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Server=NT-04871;Database=master;Initial Catalog=CatalogoDB;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoApi.Models
+{
+    public class Produto
+    {
+        // Always define Length of string
+        [Key]
+        public int ProdutoId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Descricao { get; set; }
+        [Required]
+        public decimal Preco { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public double Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
